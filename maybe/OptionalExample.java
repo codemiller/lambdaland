a2387d3d4b37b120cf85b232b450eb06e9874d10
Katie Miller <katie@codemiller.com>
2013-01-06 12:17:37 +1000

    Improved Optional example

--- maybe/OptionalExample.java
+++ maybe/OptionalExample.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import static com.google.common.collect.Maps.newHashMap;
 
 /**
+ * Example showing use of Google Guava's Optional type.
+ *
  * @author kamiller@redhat.com (Katie Miller)
  */
 public class OptionalExample {
@@ -15,72 +17,33 @@ public class OptionalExample {
         petEnclosures.put(102, "Fluffy");
         petEnclosures.put(103, null);
         petEnclosures.put(104, "Felix");
-        petEnclosures.put(105, null);
-        petEnclosures.put(106, "Ra");
 
-        if (args.length != 1) {
+        int enclosure = -1;
+        try {
+            enclosure = Integer.parseInt(args[0]);
+        } catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
             System.err.println("Error: supply number of enclosure to check, ie: 101");
             System.exit(1);
         }
-        int enclosure = Integer.parseInt(args[0]);
 
-        // Null here could mean there is no enclosure with that number or it doesn't contain a pet
+        // The meaning of a null result here is ambiguous
         String petName = petEnclosures.get(enclosure);
-        printStatus(enclosure, petName);
+        System.out.println("Enclosure " + enclosure + " contains " + petName + ".");
 
         // This time there is no ambiguity
-        Optional<String> enclosureContents = checkEnclosure(petEnclosures, enclosure);
-        if (! enclosureContents.isPresent()) {
+        Optional<Optional<String>> enclosureValue = doLookup(petEnclosures, enclosure);
+        if (! enclosureValue.isPresent()) {
             System.err.println("Error: invalid enclosure number");
             System.exit(1);
         }
-        petName = enclosureContents.get();
-        printStatus(enclosure, petName);
-
-        // A nullable value can be turned into an Optional with Optional.fromNullable
-        Optional<String> popularityLevel = Optional.fromNullable(ratePetNamePopularity(petName));
-        if ((!pet.equals("nothing"))
-                && popularityLevel.isPresent()) {
-            System.out.println("Pet name's popularity is " + popularityLevel.get() + ".");
-        }
+        petName = enclosureValue.get().or("nothing");
+        System.out.println("Enclosure " + enclosure + " contains " + petName + ".");
     }
 
-    // Returning an Optional forces the coder to think about cases when a value should be represented as present,
-    // and when it should be represented as absent, and encourages the users of this method to consider what
-    // to do when a value is absent
-    private static Optional<String> checkEnclosure(Map<Integer, String> petEnclosures, int enclosure) {
-        if (! petEnclosures.containsKey(enclosure)) {
+    private static <K, V> Optional<Optional<V>> doLookup(Map<K, V> map, K key) {
+        if (! map.containsKey(key)) {
             return Optional.absent();
         }
-        String value = petEnclosures.get(enclosure);
-        if (value == null) {
-            return Optional.of("nothing");
-        }
-        return Optional.of(value);
-    }
-
-    private static void printStatus(int enclosure, String pet) {
-        System.out.println("Enclosure " + enclosure + " contains " + pet + ".");
-    }
-
-    // Returning an Optional here instead of String would make it clear that a value may not be returned in
-    // all cases and the coder should check for this
-    public static String ratePetNamePopularity(String petName) {
-        if (petName.length() < 3) {
-            return null;
-        }
-        int nameScore = 0;
-        if (petName.length() > 5) {
-            nameScore += 5;
-        }
-        if (petName.contains("o") || petName.contains("i")) {
-            nameScore += 2 + petName.charAt(2);
-        }
-        if (petName.equals("Morpheus") || petName.equals("Neo")) {
-            nameScore += 20;
-        }
-        if (nameScore < 10) return "low";
-        if (nameScore > 120) return "high";
-        return "average";
+        return Optional.of(Optional.fromNullable(map.get(key)));
     }
 }
