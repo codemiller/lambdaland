1c59e7d0641515e92fc3ce5dfec5f2d6a2f54432
Steve Dalton <steve@refactor.com.au>
2013-01-13 22:26:46 +1000

    Added immutable

--- immutability/dog.php
+++ immutability/dog.php
@@ -1,27 +1,29 @@
 <?php
+class Dog
+{
+  private $name;
 
-class Dog {
-    private $name;
+  public function __construct($name)
+  {
+    $this->name = $name;
+  }
 
-    public function __construct($name) {
-        $this->name = $name;
+  public function __get($key)
+  {
+    if (property_exists($this, $key)) {
+      return $this->{$key};
+    } else {
+      throw new \RuntimeException("Property doesn't exist");
     }
+  }
 
-    public function __get($key) {
-        if (property_exists($this, $key)) {
-            return $this->{$key};
-        } else {
-            throw new \RuntimeException('Property doesn't exist');
-        }
-    }
-
-    public function __set($key, $value) {
-        throw new \RuntimeException('This object is immutable');
-    }
+  public function __set($key, $value)
+  {
+    throw new \RuntimeException("This object is immutable");
+  }
 }
 
+$billy = new Dog("Billy");
+$billy->name = "Buster"; // no dice!
 
-$billy = new Dog('Billy');
-var_dump($billy->name);
-$billy->name = 'Buster';
 ?>
